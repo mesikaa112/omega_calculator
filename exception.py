b39ca956fdc29f2264ec1d70c9b9a58f7491ecc7
yonatan mesika <yonatan11mesika@gmail.com>
2022-11-26 20:50:13 +0200

    added exception class

---
+++ exception.py
@@ -0,0 +1,70 @@
+class Error(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class PlusError(Error):
+    def __init__(self, message="there is an ERROR! the plus sign is the cause"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class MinusError(Error):
+    def __init__(self, message="there is an ERROR! the minus sign is the cause"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class MultiplyError(Error):
+    def __init__(self, message="there is an ERROR! the multiply sign is the cause"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class DivideError(Error):
+    def __init__(self, message="there is an ERROR! the divide sign is the cause"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class PowerError(Error):
+    def __init__(self, message="there is an ERROR! the power sign is the cause"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ModuloError(Error):
+    def __init__(self, message="there is an ERROR! the modulo sign is the cause"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class MaximumError(Error):
+    def __init__(self, message="there is an ERROR! the maximum sign is the cause"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class MinimumError(Error):
+    def __init__(self, message="there is an ERROR! the minimum sign is the cause"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class AverageError(Error):
+    def __init__(self, message="there is an ERROR! the average sign is the cause"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class NegativeError(Error):
+    def __init__(self, message="there is an ERROR! the tilde sign is the cause"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class FactorialError(Error):
+    def __init__(self, message="there is an ERROR! the factorial sign is the cause"):
+        self.message = message
+        super().__init__(self.message)
