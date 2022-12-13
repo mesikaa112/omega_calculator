7a587838ff4af4a946e3c5df039e8778b893bc4d
yonatan mesika <yonatan11mesika@gmail.com>
2022-12-13 12:04:17 +0200

    added more generics in substring invalid characters like space

--- const.py
+++ const.py
@@ -2,10 +2,12 @@ __all__ = [
     'OPERATORS_PRIORITY',
     'BETWEEN_ONE_OPERATORS',
     'BETWEEN_TWO_OPERATORS',
-    'VALID_CHARACTERS'
+    'VALID_CHARACTERS',
+    'INVALID_CHARACTERS'
 ]
 
 OPERATORS_PRIORITY = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, '%': 4, '$': 5, '&': 5, '@': 5, '~': 6, '!': 6}
 BETWEEN_TWO_OPERATORS = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, '%': 4, '$': 5, '&': 5, '@': 5}
 BETWEEN_ONE_OPERATORS = {'~': 6, '!': 6}
 VALID_CHARACTERS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '!', '.', '@', '#', '$', '%', '^', '&', '*', '(', ')', '~', ' '}
+INVALID_CHARACTERS = {' '}
