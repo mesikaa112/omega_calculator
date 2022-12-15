af187c054191aa9816d2a2a51bfd83e18e607223
yonatan mesika <yonatan11mesika@gmail.com>
2022-12-15 17:35:39 +0200

    added turn from string to list and turn from infix to postfix

--- substring_number.py
+++ substring_number.py
@@ -1,5 +1,6 @@
 from const import *
-from validation import is_unary_minus
+from validation import is_unary_minus, is_char_in_number, is_number
+import re
 
 
 def is_operator2_negative_number(equation: str, index: int, buff: int) -> bool:
@@ -59,8 +60,8 @@ def remove_duplicated_minuses(equation: str) -> str:
 def replace_minuses(equation: str, first_minus_index: int, last_minus_index: int, countNegatives: int) -> str:
     if countNegatives % 2 == 0:
         # if is unary minus, delete all the minuses else replace all the minuses with plus sign
-        if is_unary_minus(equation, first_minus_index):\
-            return equation[:first_minus_index] + equation[last_minus_index + 1:]
+        if is_unary_minus(equation, first_minus_index): \
+                return equation[:first_minus_index] + equation[last_minus_index + 1:]
         else:
             return equation[:first_minus_index] + '+' + equation[last_minus_index + 1:]
     # replace all the minuses with one minus
@@ -120,7 +121,8 @@ def substring_number(equation: str, index: int) -> (str, str):
     while (index + buff) < len(equation) and is_operator2_negative_number(equation, index, buff):
         operand2 += equation[index + buff]
         buff += 1
-    while (index + buff) < len(equation) and (is_number(equation[index + buff]) or is_float_number(equation, (index + buff))):
+    while (index + buff) < len(equation) and (
+            is_number(equation[index + buff]) or is_float_number(equation, (index + buff))):
         operand2 += equation[index + buff]
         buff += 1
     operand2 = remove_duplicated_minuses(equation, operand2)
@@ -174,7 +176,8 @@ def get_index_for_operand2(equation: str, index: int) -> int:
     while (index + buff) < len(equation) and is_operator2_negative_number(equation, index, buff):
         index_of_operand2 += 1
         buff += 1
-    while (index + buff) < len(equation) and (is_number(equation[index + buff]) or is_float_number(equation, (index + buff))):
+    while (index + buff) < len(equation) and (
+            is_number(equation[index + buff]) or is_float_number(equation, (index + buff))):
         index_of_operand2 += 1
         buff += 1
     return index_of_operand2
@@ -218,3 +221,70 @@ def substring_float_to_int(operand: str) -> int:
         if operand[i] == '.':
             return int(operand[: i])
     return int(operand)
+
+
+def add_equation_to_list(equation: str) -> list:
+    """
+    this method add an equation into list in the same order
+    :param equation: the equation in str type
+    :return: the equation in str type
+    """
+    equation_list = []
+    is_first_number = True
+    for char in equation:
+        # if the char is a number or '.'
+        if is_number(char):
+            # if this is the first char in the number
+            if is_first_number:
+                equation_list.append(char)
+                is_first_number = False
+            # if this is not first char in the number
+            else:
+                equation_list[-1] += char
+        # if the char is an operator
+        else:
+            equation_list.append(char)
+            is_first_number = True
+
+    return equation_list
+
+
+def infix_to_postfix(equation_list: list) -> list:
+    """
+    this method turns the equation list from infix to postfix
+    :param equation_list: the equation in infix in list type
+    :return: returns a list in of the equation in postfix
+    """
+    new_equation_list = []
+    operators_stack = []
+    for character in equation_list:
+        # if character is number
+        if is_number(character):
+            new_equation_list.append(character)
+
+        # else the character is an operator or ()
+        else:
+            if character == '(':
+                operators_stack.append(character)
+
+            elif character == ')':
+                operator = operators_stack.pop()
+                while operator != '(':
+                    new_equation_list.append(operator)
+                    operator = operators_stack.pop()
+
+            # if the character is not ( or ) than the item must be an operator
+            else:
+                # appending the operators from the stack as long as they have a higher priority than the current
+                # operator and the current top character is not ( and the stack isn't empty
+                while len(operators_stack) > 0 and operators_stack[-1] != '(' \
+                        and OPERATORS_PRIORITY[operators_stack[-1]] >= OPERATORS_PRIORITY[character]:
+                    new_equation_list.append(operators_stack.pop())
+
+                operators_stack.append(character)
+
+    # at the end just append all of the remaining operators to the new list
+    while len(operators_stack) > 0:
+        new_equation_list.append(operators_stack.pop())
+
+    return new_equation_list
