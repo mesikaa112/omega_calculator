3fd4428d3a2bc7d702d2e5b15c22b44d5d2e63c6
yonatan mesika <yonatan11mesika@gmail.com>
2022-12-17 12:12:08 +0200

    added # operator

--- const.py
+++ const.py
@@ -3,11 +3,14 @@ __all__ = [
     'BETWEEN_ONE_OPERATORS',
     'BETWEEN_TWO_OPERATORS',
     'VALID_CHARACTERS',
-    'INVALID_CHARACTERS'
+    'INVALID_CHARACTERS',
+    'OPERATORS_LIST'
 ]
 
-OPERATORS_PRIORITY = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, '%': 4, '$': 5, '&': 5, '@': 5, '~': 6, '!': 6}
+OPERATORS_PRIORITY = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, '%': 4, '$': 5, '&': 5, '@': 5, '~': 6, '!': 6, '#': 6}
+OPERATORS_LIST = {'+', '-', '*', '/', '^', '%', '$', '&', '@', '~', '!', '#', '(', ')'}
 BETWEEN_TWO_OPERATORS = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, '%': 4, '$': 5, '&': 5, '@': 5}
-BETWEEN_ONE_OPERATORS = {'~': 6, '!': 6}
-VALID_CHARACTERS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '!', '.', '@', '#', '$', '%', '^', '&', '*', '(', ')', '~', ' '}
+BETWEEN_ONE_OPERATORS = {'~': "left", '!': "right", '#': "right"}
+VALID_CHARACTERS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '!', '.', '@', '#', '$', '%', '^',
+                    '&', '*', '(', ')', '~', ' '}
 INVALID_CHARACTERS = {' '}
